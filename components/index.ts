28f7ab5d7aa57e59cb09770dd75a9551f3fc4fae
yhunlu <yhunlu87@gmail.com>
2023-06-27 23:52:07 +0300

    redesigned-Hero section, boiler-plate navbar, footer.

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,4 @@
 export { default as Hero } from './Hero';
 export { default as CustomButton } from './CustomButton';
+export { default as NavBar } from './NavBar';
+export { default as Footer } from './Footer';
