35ea94966ab916043fa13879d3f7a627a7cfaabd
yhunlu <yhunlu87@gmail.com>
2023-06-30 01:12:14 +0300

    feat-searchbar.

---
+++ components/SearchBar.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { SearchManufacturer } from '@/components'
+import { useState } from 'react'
+import { manufacturers } from './../constants/index';
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+
+    const handleSearch = () => {
+
+    }
+
+    return (
+        <form className="searchbar" onSubmit={handleSearch}>
+            <div className="searchbar__item">
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    )
+}
+
+export default SearchBar
