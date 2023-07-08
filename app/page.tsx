bb37cfa73a3cc8559a3db9ca80813457ce4a0be8
yhunlu <yhunlu87@gmail.com>
2023-07-09 01:24:49 +0300

    refactoring fetchCars api for getting data from adress bar.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
+import { HomeProps } from '@/types';
 import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -28,12 +35,14 @@ export default async function Home() {
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car) => <CarCard car={car} />)}
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
             </div>
           </section>
         ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
         )}
--- types/index.ts
+++ types/index.ts
@@ -33,3 +33,15 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
